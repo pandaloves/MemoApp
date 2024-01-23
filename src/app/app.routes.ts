7b57fba350694f10c07ca9ccdf735d88ea795e63
pandaloves <meiyinghu18@gmail.com>
2024-01-23 22:39:53 +0100

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,54 @@
 import { Routes } from '@angular/router';
+import { BooksComponent } from './components/books/books.component';
+import { BookFormComponent } from './components/books/book-form/book-form.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+import { QuoteFormComponent } from './components/quotes/quote-form/quote-form.component';
+import { LoginComponent } from './components/login/login.component';
+import { EmployeeListComponent } from './components/employee/employee-list.component';
+import { EmployeeFormComponent } from './components/employee/employee-form/employee-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'books', pathMatch:'full'},
+    
+    {
+        path: 'books',
+        component: BooksComponent
+    },
+    {
+        path: 'book/form',
+        component: BookFormComponent
+    },
+    {
+        path: 'books/:id',
+        component: BookFormComponent
+    },
+    {
+        path: 'quotes',
+        component: QuotesComponent
+    },
+    {
+        path: 'quote/form',
+        component: QuoteFormComponent
+    },
+    {
+        path: 'quotes/:id',
+        component: QuoteFormComponent
+    },
+    {
+        path: 'employee-list',
+        component: EmployeeListComponent,
+      },
+      {
+        path: 'create-employee',
+        component: EmployeeFormComponent,
+      },
+      {
+        path: 'employee/:id',
+        component: EmployeeFormComponent,
+      },
+    {
+        path: 'login',
+        component: LoginComponent,
+      },
+
+];
---
+++ src/app/components/books/books.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import {BooksService} from '../../services/books.service';
+import {Book} from '../../types/book';
+import {AsyncPipe} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-books',
+  standalone: true,
+  imports: [AsyncPipe,RouterLink],
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.css'
+})
+export class BooksComponent implements OnInit{
+  books$!:Observable<Book[]>
+  toaster = inject(ToastrService);
+  bookService = inject(BooksService);
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  delete(id:number) {
+    this.bookService.deleteBook(id).subscribe({
+      next:(response)=> {
+      this.toaster.success('Book successfully deleted!');
+      this.getBooks();
+      }
+    });
+  }
+
+  private getBooks():void {
+    this.books$ = this.bookService.getBooks()
+  }
+}
